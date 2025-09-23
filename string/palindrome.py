2cbac1447dcb1a0b51bd02bb9a47dfc82e94eb68
Sayed Mohammad Taoos <taoos0001@gmail.com>
2025-09-23 19:31:33 +0530

    Add files via upload

---
+++ string/palindrome.py
@@ -0,0 +1,14 @@
+def is_palindrome(s):
+    i=0
+    j=len(s)-1
+
+    while i < j :
+        if s[i] != s[j]:
+            return False
+        
+        i+=1
+        j-=1
+
+        return True
+
+print(is_palindrome('Heheh'))
